e488d9fdd2a1a216e05545f105d38921f02d10fd
offercantrefuse <smjdent@gmail.com>
2019-04-19 21:59:02 +0900

    boj 17135

--- BOJ/백준 17135 캐슬 디펜스.cpp
+++ BOJ/백준 17135 캐슬 디펜스.cpp
@@ -25,32 +25,32 @@ void simul() {
 	cnt = 0;
 	int yy = N;
 	vector<pos> vec;
-	for (int tt = 0; tt < N; tt++) {
+	for (int tt = 0; tt < N; tt++) {  // row 수만큼
 		for (int k = 0; k < 3; k++) {
 			bool found = false;
 			int xx = arr[k];
 			for (int d = 1; d <= D && !found; d++) {
-				for (int i = yy - 1; i >= yy - d && i >= 0 && !found; i--) {
+				for (int i = yy - 1; i >= yy - d && i >= 0 && !found; i--) {   // 거리 d 에 따라 왼쪽부터 오른쪽 위로 올라감
 					if (xx - (d - (yy - i)) < 0) continue;
-					if (copied[i][xx - (d - (yy - i))] == 1) {
+					if (copied[i][xx - (d - (yy - i))] == 1) {  // 찾으면 2로 바꿈
 						copied[i][xx - (d - (yy - i))] = 2;
 						cnt++;
 						found = true;
-						vec.push_back(pos(xx - (d - (yy - i)), i));
+						vec.push_back(pos(xx - (d - (yy - i)), i));   // 찾은 좌표 vector에 넣어줌
 					}
 					else if (copied[i][xx - (d - (yy - i))] == 2) {
 						found = true;
 					}
 				}
-				if (found) break;
-				for (int i = yy - d + 1; i <= yy - 1 && !found; i++) {
+				if (found) break;   // 이미 찾았으면 break
+				for (int i = yy - d + 1; i <= yy - 1 && !found; i++) {  // 거리 d에 따라 위쪽부터 오른쪽 아래로 내려옴 
 					if (i < 0) continue;
 					if (xx + (d - (yy - i)) > M - 1) continue;
 					if (copied[i][xx + (d - (yy - i))] == 1) {
 						copied[i][xx + (d - (yy - i))] = 2;
 						cnt++;
 						found = true;
-						vec.push_back(pos(xx + (d - (yy - i)), i));
+						vec.push_back(pos(xx + (d - (yy - i)), i));  // 찾은 좌표 vector에 넣어줌
 					}
 					else if (copied[i][xx + (d - (yy - i))] == 2) {
 						found = true;
@@ -59,7 +59,7 @@ void simul() {
 
 			}
 		}
-		for (auto v : vec) {
+		for (auto v : vec) {  // 2로 바꿔줬던 좌표들 0으로
 			copied[v.y][v.x] = 0;
 		}
 		vec.clear();
