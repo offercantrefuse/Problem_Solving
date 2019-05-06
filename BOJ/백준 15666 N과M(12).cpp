d79f678978f4fa1cef6a712a177da1bb74de9812
offercantrefuse <smjdent@gmail.com>
2019-05-06 18:39:55 +0900

    boj 15666

---
+++ BOJ/백준 15666 N과M(12).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+
+int N, M;
+set<int> tmp_set;
+vector<int> arr;
+int ans[8];
+
+void dfs(int start, int idx, int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << ans[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = start; i < arr.size(); i++) {
+		ans[idx] = arr[i];
+		dfs(i, idx + 1, cnt + 1);
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		tmp_set.insert(tmp);
+	}
+	for (auto tt : tmp_set)
+		arr.push_back(tt);
+	sort(arr.begin(), arr.end());
+	dfs(0, 0, 0);
+	return 0;
+}
