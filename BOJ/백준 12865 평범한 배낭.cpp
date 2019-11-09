c1a6677e5b4452ade5a1ba19b6d2f9c3d4fec16c
offercantrefuse <smjdent@gmail.com>
2019-11-09 13:39:34 +0900

    boj 12865

---
+++ BOJ/백준 12865 평범한 배낭.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N, K;
+int W[101], V[101], dp[101][100001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> W[i] >> V[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j <= K; j++) {
+			if (j - W[i] < 0) {
+				dp[i][j] = dp[i - 1][j];
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+			}
+		}
+	}
+	cout << dp[N][K];
+	return 0;
+}
